b39e0f386a69b46c3fdb14a4aad122a674dc534b
Christian Maniewski <code@chmanie.com>
2020-02-13 22:56:51 +0100

    Add Program query resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -6,6 +6,7 @@ import { Query } from './Query'
 import { Colony } from './Colony'
 import { Domain } from './Domain'
 import { PersistentTask } from './PersistentTask'
+import { Program } from './Program'
 import { Suggestion } from './Suggestion'
 import { Submission } from './Submission'
 import { User } from './User'
@@ -22,6 +23,7 @@ export const resolvers: Resolvers<ApolloContext> = {
   Query,
   Domain,
   PersistentTask,
+  Program,
   Suggestion,
   User,
   Task,
