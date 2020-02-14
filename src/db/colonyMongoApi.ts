a4a4cfbe5d95e11f703c9ed420ead75da8ce5c51
Christian Maniewski <code@chmanie.com>
2020-02-14 17:26:49 +0100

    Add Program integration tests

--- src/db/colonyMongoApi.ts
+++ src/db/colonyMongoApi.ts
@@ -1292,12 +1292,10 @@ export class ColonyMongoApi {
       title,
       description,
       status,
-      $push,
     }: {
       title?: string
       description?: string
       status?: ProgramStatus
-      $push?: { enrolledUsers: string }
     },
   ) {
     await this.tryGetUser(initiator)
@@ -1307,7 +1305,6 @@ export class ColonyMongoApi {
       title?: string
       description?: string
       status?: ProgramStatus
-      $push?: { enrolledUsers: string }
     }
 
     if (title) {
@@ -1319,15 +1316,24 @@ export class ColonyMongoApi {
     if (status) {
       update.status = status
     }
-    if ($push) {
-      update.$push = $push
-    }
 
     return this.programs.updateOne(
       {
         _id: new ObjectID(id),
       },
       { $set: update },
+    )
+  }
+
+  async enrollInProgram(initiator: string, id: string) {
+    await this.tryGetUser(initiator)
+    await this.tryGetProgram(id)
+
+    return this.programs.updateOne(
+      {
+        _id: new ObjectID(id),
+      },
+      { $push: { enrolledUserAddresses: initiator } },
     )
   }
 
