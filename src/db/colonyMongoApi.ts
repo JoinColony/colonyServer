f8d85a0108b1f7dbc8752509cc11b8e3cb841f28
Christian Maniewski <code@chmanie.com>
2020-03-17 14:10:01 +0100

    Also notify task creator

--- src/db/colonyMongoApi.ts
+++ src/db/colonyMongoApi.ts
@@ -1196,7 +1196,9 @@ export class ColonyMongoApi {
     const { creatorAddress: programCreator } = await this.tryGetProgram(
       programId.toString(),
     )
-    await this.tryGetPersistentTask(persistentTaskId)
+    const { creatorAddress: taskCreator } = await this.tryGetPersistentTask(
+      persistentTaskId,
+    )
 
     const existingSubmission = await this.submissions.findOne({
       creatorAddress: initiator,
@@ -1226,7 +1228,7 @@ export class ColonyMongoApi {
         submissionId,
       },
     )
-    await this.createNotification(eventId, [programCreator, levelCreator])
+    await this.createNotification(eventId, [programCreator, levelCreator, taskCreator])
 
     return submissionId
   }
