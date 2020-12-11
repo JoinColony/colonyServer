f1ed25d96b161760137f5367a4d67f75be038212
Alex <git@area.io>
2020-12-11 12:08:40 +0000

    Updates to graph and setup scripts

--- scripts/setupGraph.sh
+++ scripts/setupGraph.sh
@@ -2,4 +2,8 @@ mkdir -p ./data/ipfs
 mkdir -p ./data/postgres
 if ! grep 'user: "$UID"' ./docker-compose.yml; then
     sed -i.bak 's/  postgres:/  postgres:\n    user: "$UID"/g' ./docker-compose.yml
+    sed -i.bak "s/  graph-node:/  graph-node:\n    extra_hosts:\n      - \"host.docker.internal:$(hostname -I | awk '{print $1}')\"/" ./docker-compose.yml
+fi
+if ! grep '-h 0.0.0.0' ./../../colonyNetwork/scripts/start-blockchain-client.sh; then
+    sed -i.bak 's/ganache-cli/ganache-cli -h 0.0.0.0/g' ./../../colonyNetwork/scripts/start-blockchain-client.sh
 fi
