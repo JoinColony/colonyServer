bbc39520ef1aab7f54d8b171d07a20047e25c12c
Alicja Kujawa <alicjak94.ak@gmail.com>
2021-02-25 16:47:10 +0100

    Update event context

--- src/graphql/eventContext.d.ts
+++ src/graphql/eventContext.d.ts
@@ -1,46 +1,12 @@
 import { EventType } from '../graphql/types'
 import {
-  AssignWorkerEvent,
-  CancelTaskEvent,
   CreateDomainEvent,
-  CreateTaskEvent,
-  CreateWorkRequestEvent,
-  FinalizeTaskEvent,
   NewUserEvent,
-  RemoveTaskPayoutEvent,
-  SendWorkInviteEvent,
-  SetTaskDescriptionEvent,
-  SetTaskDomainEvent,
-  SetTaskDueDateEvent,
-  SetTaskPayoutEvent,
-  SetTaskPendingEvent,
-  SetTaskSkillEvent,
-  RemoveTaskSkillEvent,
-  SetTaskTitleEvent,
-  TaskMessageEvent,
-  UnassignWorkerEvent,
   TransactionMessageEvent,
 } from './types'
 
 interface EventContextMap {
-  [EventType.AssignWorker]: AssignWorkerEvent
-  [EventType.CancelTask]: CancelTaskEvent
   [EventType.CreateDomain]: CreateDomainEvent
-  [EventType.CreateTask]: CreateTaskEvent
-  [EventType.CreateWorkRequest]: CreateWorkRequestEvent
-  [EventType.FinalizeTask]: FinalizeTaskEvent
-  [EventType.RemoveTaskPayout]: RemoveTaskPayoutEvent
-  [EventType.SendWorkInvite]: SendWorkInviteEvent
-  [EventType.SetTaskDescription]: SetTaskDescriptionEvent
-  [EventType.SetTaskDomain]: SetTaskDomainEvent
-  [EventType.SetTaskDueDate]: SetTaskDueDateEvent
-  [EventType.SetTaskPayout]: SetTaskPayoutEvent
-  [EventType.SetTaskPending]: SetTaskPendingEvent
-  [EventType.SetTaskSkill]: SetTaskSkillEvent
-  [EventType.RemoveTaskSkill]: RemoveTaskSkillEvent
-  [EventType.SetTaskTitle]: SetTaskTitleEvent
-  [EventType.TaskMessage]: TaskMessageEvent
-  [EventType.UnassignWorker]: UnassignWorkerEvent
   [EventType.NewUser]: NewUserEvent
   [EventType.TransactionMessage]: TransactionMessageEvent
 }
